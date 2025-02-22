08d70ff5dfa478ce70fad49ddf29b401b137f64b
Prayas <prayaspal04@gmail.com>
2025-02-22 22:07:16 +0530

    more middleware

--- client/middleware.ts
+++ client/middleware.ts
@@ -26,5 +26,5 @@ export function middleware(request: NextRequest) {
 
 // See "Matching Paths" below to learn more
 export const config = {
-    matcher: ['/']
+    matcher: ['/', '/creator/*', '/brand', '/reader/*', 'token/']
 }
